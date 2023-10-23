2c583b60c6c24538c86da58a8066e6c515dbd9b8
leodor-tom <eleodoritom@gmail.com>
2023-10-23 15:01:31 +0200

    refactor posizione mezzo.java

--- src/main/java/BuildWeek1/entities/Mezzi.java
+++ src/main/java/BuildWeek1/entities/Mezzi.java
@@ -1,65 +1,68 @@
-package entities;
+package BuildWeek1.entities;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
-import javax.persistence.Entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
-import javax.persistence.*;
 @Entity
-@Table(name="mezzi")
+@Table(name = "mezzi")
 public class Mezzo {
     @Id
     @GeneratedValue
     private long id;
     @Enumerated(EnumType.STRING)
-    @Column (name = "tipo_mezzo",nullable = false)
+    @Column(name = "tipo_mezzo", nullable = false)
     private TipoMezzo tipoMezzo;
-    @Column (name="numero_posti")
+    @Column(name = "numero_posti")
 
     private long numeroPosti;
-    @Column (name="in_manutenzione")
+    @Column(name = "in_manutenzione")
     private boolean inManutenzione;
-    @Column (name="tempo_manutenzione")
+    @Column(name = "tempo_manutenzione")
 
-private long tempoManutenzione;
-    @Column (name="in_servizio")
-private boolean inServizio;
-@ManyToMany
-    @JoinTable(name="user_mezzi",joinColumns = @JoinColumn(name="mezzo_id"),inverseJoinColumns = @JoinColumn(name = "user_id"))
-private Set<User> user;
+    private long tempoManutenzione;
+    @Column(name = "in_servizio")
+    private boolean inServizio;
+    @ManyToMany
+    @JoinTable(name = "user_mezzi", joinColumns = @JoinColumn(name = "mezzo_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> user;
 
 
-private List<LocalDate> dataInizioManutenzione;
+    private List<LocalDate> dataInizioManutenzione;
     @ManyToMany
     @JoinTable(name = "tickets_mezzi",
-            joinColumns =@JoinColumn(name = "mezzo_id") ,
+            joinColumns = @JoinColumn(name = "mezzo_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private Set<Ticket> tickets;
 
 
-
     @ManyToMany
-    @JoinTable (name="mezzi_tratte",
-    joinColumns = @JoinColumn(name="mezzo_id"),
-    inverseJoinColumns = @JoinColumn(name = "tratta_id"))
-    private Set <Tratta> tratta;
+    @JoinTable(name = "mezzi_tratte",
+            joinColumns = @JoinColumn(name = "mezzo_id"),
+            inverseJoinColumns = @JoinColumn(name = "tratta_id"))
+    private Set<Tratta> tratta;
 
 
+    private List<LocalDate> dataFineManutenzione;
 
-            private List<LocalDate> dataFineManutenzione;
+    private List<LocalDate> dataInizioServizio;
 
-            private List<LocalDate> dataInizioServizio;
-
-            private List<LocalDate> dataFineServizio;
+    private List<LocalDate> dataFineServizio;
 
     public Mezzo() {
+    }
+
+    public Mezzo(long id, boolean inManutenzione, long tempoManutenzione, boolean inServizio, List<LocalDate> dataInizioManutenzione, List<LocalDate> dataFineManutenzione, List<LocalDate> dataInizioServizio, List<LocalDate> dataFineServizio) {
+        this.id = id;
+        this.inManutenzione = inManutenzione;
+        this.tempoManutenzione = tempoManutenzione;
+        this.inServizio = inServizio;
+        this.dataInizioManutenzione = dataInizioManutenzione;
+        this.dataFineManutenzione = dataFineManutenzione;
+        this.dataInizioServizio = dataInizioServizio;
+        this.dataFineServizio = dataFineServizio;
     }
 
     public long getId() {
@@ -103,7 +106,7 @@ private List<LocalDate> dataInizioManutenzione;
     }
 
     public void setDataFineManutenzione(LocalDate dataFineManutenzione) {
-        this.dataFineManutenzione.add(dataFineManutenzione) ;
+        this.dataFineManutenzione.add(dataFineManutenzione);
     }
 
     public List<LocalDate> getDataInizioServizio() {
@@ -120,16 +123,5 @@ private List<LocalDate> dataInizioManutenzione;
 
     public void setDataFineServizio(LocalDate dataFineServizio) {
         this.dataFineServizio.add(dataFineServizio);
-    }
-
-    public Mezzo(long id, boolean inManutenzione, long tempoManutenzione,boolean inServizio, List<LocalDate> dataInizioManutenzione, List<LocalDate> dataFineManutenzione, List<LocalDate> dataInizioServizio, List<LocalDate> dataFineServizio) {
-        this.id = id;
-        this.inManutenzione = inManutenzione;
-        this.tempoManutenzione = tempoManutenzione;
-        this.inServizio = inServizio;
-        this.dataInizioManutenzione = dataInizioManutenzione;
-        this.dataFineManutenzione = dataFineManutenzione;
-        this.dataInizioServizio = dataInizioServizio;
-        this.dataFineServizio = dataFineServizio;
     }
 }
---
+++ src/main/java/BuildWeek1/entities/TipoMezzo.java
@@ -0,0 +1,5 @@
+package BuildWeek1.entities;
+
+public enum TipoMezzo {
+    TRAM, AUTOBUS
+}
